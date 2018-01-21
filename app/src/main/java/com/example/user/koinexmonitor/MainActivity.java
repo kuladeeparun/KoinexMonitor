f2f09b736d05e58b2647c6dee914f4d05f20c2aa
Kuladeep <kuladeeparun@gmail.com>
2018-01-21 14:11:01 +0530

    Removed AsyncTask, set the AlarmManager, button just sits there now. Still a single activity application

--- app/src/main/java/com/example/user/koinexmonitor/MainActivity.java
+++ app/src/main/java/com/example/user/koinexmonitor/MainActivity.java
@@ -1,121 +1,89 @@
 package com.example.user.koinexmonitor; 
 
 
-        import android.app.IntentService;
-        import android.app.NotificationManager;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.net.ConnectivityManager;
-        import android.net.NetworkInfo;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.support.annotation.Nullable;
-        import android.support.v4.app.NotificationCompat;
-        import android.support.v7.app.AppCompatActivity;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.ArrayAdapter;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.ProgressBar;
-        import android.widget.Spinner;
-        import android.widget.TextView;
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-        import org.json.JSONArray;
-        import org.json.JSONException;
-        import org.json.JSONObject;
-        import org.json.JSONTokener;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.net.HttpURLConnection;
-        import java.net.MalformedURLException;
-        import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //EditText emailText;
-    TextView responseView;
     TextView bitcoinValue, rippleValue, ethereumValue;
-    EditText alertThreshold;
-    ProgressBar progressBar;
-    Spinner currencies;
-
-    //static final String API_KEY = "USE_YOUR_OWN_API_KEY";
     static final String API_URL = "https://koinex.in/api/ticker";
-    //static final String API_URL = "http://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //responseView = findViewById(R.id.responseView);
-        //emailText = (EditText) findViewById(R.id.emailText);
-        progressBar = findViewById(R.id.progressBar);
-
         bitcoinValue = findViewById(R.id.bitcoinValue);
         rippleValue = findViewById(R.id.rippleValue);
         ethereumValue = findViewById(R.id.ethereumValue);
 
-        //alertThreshold = findViewById(R.id.alertThreshold);
-
-        //currencies = findViewById(R.id.spinner);
-        /*ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.currencies, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        currencies.setAdapter(adapter);*/
-
         Button queryButton = findViewById(R.id.queryButton);
-        queryButton.setOnClickListener(new View.OnClickListener() {
+        /*queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new RetrieveFeedTask().execute();
-
+                scheduleAlarm();
             }
-        });
+        });*/
+        scheduleAlarm();
     }
 
-    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
-        private Exception exception;
+    public void scheduleAlarm() {
 
-       // Intent in = new Intent(getApplicationContext(), BackgroundService.class);
+        Intent intent = new Intent(getApplicationContext(), BackgroundService.class);
+        final PendingIntent pIntent = PendingIntent.getService(this, BackgroundService.REQUEST_CODE,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long firstMillis = System.currentTimeMillis();
+        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,
+                AlarmManager.INTERVAL_FIFTEEN_MINUTES, pIntent);
+    }
 
 
-        protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
-            //responseView.setText("Fetching data...");
+    class BackgroundService extends IntentService {
+
+        public static final int REQUEST_CODE = 12345;
+        public BackgroundService(String name) {
+            super(name);
         }
 
-        protected String doInBackground(Void... urls) {
-            //String email = emailText.getText().toString();
-            // Do some validation here
+        String response;
 
+        @Override
+        protected void onHandleIntent(@Nullable Intent intent) {
             try {
-                URL url = new URL(API_URL);// + "email=" + email + "&apiKey=" + API_KEY);
+                URL url = new URL(API_URL);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
                 urlConnection.setReadTimeout(3000);
                 urlConnection.setConnectTimeout(3000);
                 urlConnection.setRequestMethod("GET");
                 urlConnection.setDoInput(true);
-
-
-
-                /*
-                boolean connected = false;
-                ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-                if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-                    //we are connected to a network
-                    connected = true;
-                }
-                else
-                    connected = false;
-                */
 
                 urlConnection.connect();
                 int responseCode = urlConnection.getResponseCode();
@@ -130,27 +98,25 @@ public class MainActivity extends AppCompatActivity {
                         stringBuilder.append(line).append("\n");
                     }
                     bufferedReader.close();
-                    return stringBuilder.toString();
+                    response = stringBuilder.toString();
                 }
                 finally{
                     urlConnection.disconnect();
                 }
             } catch (MalformedURLException e) {
-                return "URLexception";
+                response = null;
             } catch (IOException e) {
-                return "IOException";
+                response =  null;
             }
 
+            updateAndNotify(response);
         }
 
-        protected void onPostExecute(String response) {
+        public void updateAndNotify(String response){
             if(response == null) {
-                response = "THERE WAS AN ERROR";
+                return;
             }
-            progressBar.setVisibility(View.GONE);
-            Log.i("INFO", response);
 
-            //responseView.setText(response);
 
             String bitcoinValue2=null, rippleValue2=null, ethereumValue2 = null;
             try {
@@ -164,26 +130,21 @@ public class MainActivity extends AppCompatActivity {
                 ethereumValue.setText(ethereumValue2);
             }
             catch (JSONException e) {
-                e.printStackTrace();
+                Log.i("INFO", response);
             }
 
-            //double threshold = Double.parseDouble(alertThreshold.getText().toString());
-            double currentAmount = Double.parseDouble(rippleValue.getText().toString());
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(getApplicationContext(), "2")
+                            .setSmallIcon(R.drawable.ic_stat_attach_money)
+                            .setContentTitle("Check out the prices")
+                            .setContentText("BTC " + bitcoinValue2 + " | XRP " + rippleValue2 + " | ETH " + ethereumValue2);
 
-            //if ( (currentAmount < (threshold - 0.15*threshold)) || (currentAmount > (threshold + 0.15*threshold)) ) {
-                NotificationCompat.Builder mBuilder =
-                        new NotificationCompat.Builder(getApplicationContext(), "2")
-                                .setSmallIcon(R.drawable.ic_stat_attach_money)
-                                .setContentTitle("Check out the prices")
-                                .setContentText("BTC " + bitcoinValue2 + " | XRP " + rippleValue2 + " | ETH " + ethereumValue2);
+            int mNotificationId = 001;
 
-                int mNotificationId = 001;
+            NotificationManager mNotifyMgr =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-                NotificationManager mNotifyMgr =
-                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-                mNotifyMgr.notify(mNotificationId, mBuilder.build());
-            //}
+            mNotifyMgr.notify(mNotificationId, mBuilder.build());
         }
     }
 
